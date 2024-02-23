c48e0fd25fb79e925c434437b72137e2c0b7b82c
NotFerchi <fernadovillafuerte17@gmail.com>
2024-02-22 21:03:43 -0600

    actualizacion de java

--- Holamundo.java
+++ Holamundo.java
@@ -5,7 +5,7 @@ package com.mycompany.holamundo;
 public class Holamundo {
 
     public static void main(String[] args) {
-        System.out.println("Hello felchi!");
+        System.out.println("Hello dani!");
         
         System.out.println("jojojojo");
     }
